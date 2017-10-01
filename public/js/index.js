9fabc8935d27ac82bf39df216ea007e1849db61a
jbadan <jenna.badanowski@gmail.com>
2017-10-01 10:00:17 -0700

    added upload image functionality to homepage

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,32 @@
-
+$(document).ready( function() {
+  //upload image
+  $(document).on('change', '.btn-file :file', function() {
+		var input = $(this),
+		label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+		input.trigger('fileselect', [label]);
+	});
+	$('.btn-file :file').on('fileselect', function(event, label) {
+		var input = $(this).parents('.input-group').find(':text'),
+		log = label;
+		  if(input.length) {
+		    input.val(log);
+		  }else{
+		    if(log) alert(log);
+		  }
+  });
+		function readURL(input) {
+		  if(input.files && input.files[0]) {
+		    var reader = new FileReader();
+		    reader.onload = function (e) {
+		      $('#img-upload').attr('src', e.target.result);
+		    }
+        reader.readAsDataURL(input.files[0]);
+		  }
+		}
+		$("#imgInp").change(function(){
+		  readURL(this);
+		});
+});
 
 function processImage() {
         var subscriptionKey = '777602b32d52468babf62398a5630de4';
