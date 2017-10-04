3313f36c95fa976b31064852a5c182b98f10a4e5
jbadan <jenna.badanowski@gmail.com>
2017-10-03 17:04:35 -0700

    fixed redirect after login to go to profile instead of home

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,7 +42,7 @@ router.get('/login', function(req, res) {
 
 // FLASH
 router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
+  successRedirect: '/profile',
   failureRedirect: '/auth/login',
   failureFlash: 'Invalid username and/or password',
   successFlash: 'You have logged in'
