dba5694185b57435a0899180496926956f1d37ff
jbadan <jenna.badanowski@gmail.com>
2017-10-06 10:28:56 -0700

    transitioned from cloudinary model to image model

--- controllers/profile.js
+++ controllers/profile.js
@@ -4,7 +4,7 @@ var isLoggedIn = require("../middleware/isLoggedIn");
 var passport = require('../config/ppConfig');
 var multer = require('multer');
 var cloudinary = require('cloudinary');
-var cloudinary2 = require('cloudinary');
+// var cloudinary2 = require('cloudinary');
 var upload = multer({dest: './uploads/'});
 var router = express.Router();
 
@@ -13,11 +13,11 @@ var images = [];
 router.get('/', isLoggedIn, function(req, res){
   db.user.find({
     where: {id: req.user.id},
-    include: [db.cloudinary]
+    include: [db.image]
   })
   .then(function(user){
     if(!user) throw Error();
-    res.render('profile/index', {user: user, cloudinary2});
+    res.render('profile/index', {user: user, cloudinary});
   })
   .catch(function(error){
     res.status(400).send("error");
@@ -29,7 +29,7 @@ router.post('/', upload.single('myFile'), function(req,res){
     images = [];
 		images.push(result.public_id);
 		  db.user.findById(req.user.id).then(function(user) {
-        user.createCloudinary({
+        user.createImage({
           src: result.public_id
         }).then(function(){
         });
@@ -40,25 +40,25 @@ router.post('/', upload.single('myFile'), function(req,res){
 
 router.get('/display/:id', isLoggedIn, function(req, res){
   var imageToRender = req.params.id;
-  db.cloudinary.findById(imageToRender).then(function(image){
+  db.image.findById(imageToRender).then(function(image){
     var cloudId = image.src;
-    res.render('display', {images, cloudId, cloudinary2});
+    res.render('display', {images, cloudId, cloudinary});
   });
 });
 
-router.post('/:id', function(req,res){
-  var imageToFavorite = req.params.id;
-  db.cloudinary.findById(imageToFavorite).then(function(image){
-    image.createFavorite({
-      isFavorite: true
-    })
-  })
-  res.redirect('/profile/');
-})
+// router.post('/:id', function(req,res){
+//   var imageToFavorite = req.params.id;
+//   db.cloudinary.findById(imageToFavorite).then(function(image){
+//     image.createFavorite({
+//       isFavorite: true
+//     })
+//   })
+//   res.redirect('/profile/');
+// })
 
 router.delete('/:id', function(req, res) {
     var imageToDelete = req.params.id;
-    db.cloudinary.destroy({
+    db.image.destroy({
       where: {id: imageToDelete}
     }).then(function(){
       res.redirect('/profile/index');
